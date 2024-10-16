a2e904c7a30dfdd3f672618c61946fa0416768b2
mayurdreams <mayur.bobde@dreamsinternational.in>
2024-10-16 18:45:32 +0530

    Category change

--- src/pages/categoryList.jsx
+++ src/pages/categoryList.jsx
@@ -3,13 +3,11 @@ import { useNavigate } from "react-router-dom";
 import Table from "../comps/table";
 import useSetTimeout from "../Hooks/useDebounce";
 import { GlobalContext } from "../GlobalContext";
-import ConfirmationModal from "../comps/confirmation";
 
 export default function CategoryList() {
   const navigate = useNavigate();
   const { auth, alert, setLoading } = useContext(GlobalContext);
 
-  const [modalOpen, setModalOpen] = useState(false);
   const [categories, setCategories] = useState([]);
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(8);
@@ -54,82 +52,18 @@ export default function CategoryList() {
     setSelectAll(!selectAll);
   };
 
-  const headers = [
-    {
-      label: (
-        <input
-          className="form-check-input"
-          type="checkbox"
-          checked={selectAll}
-          onChange={handleSelectAll}
-        />
-      ),
-    },
-    { label: "Name" },
-    { label: "Actions" },
-  ];
+  const headers = [{ label: "Name" }, { label: "Actions" }];
 
   const rows = categories.map((category) => [
-    <input
-      key={category._id}
-      className="form-check-input"
-      type="checkbox"
-      checked={selectedCategories.includes(category._id)}
-      onChange={() => handleCheckboxChange(category._id)}
-    />,
     category.name,
     <button
       key={category._id}
       onClick={() => navigate(`/add-category/${category._id}`)}
       className="btn btn-primary me-1"
     >
-      View
+      Edit
     </button>,
   ]);
-
-  const deleteCategoryList = async () => {
-    if (!selectedCategories.length) {
-      alert({
-        type: "warning",
-        title: "No Selection",
-        text: "Please select at least one enquiry to delete.",
-      });
-      return;
-    }
-    setLoading(true);
-    try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/category`, {
-        method: "DELETE",
-        headers: {
-          Authorization: localStorage.getItem("auth"),
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ ids: selectedCategories }),
-      });
-
-      const { error } = await res.json();
-      if (res.ok) {
-        setCategories((prevCategory) =>
-          prevCategory.filter(
-            (category) => !selectedCategories.includes(category._id)
-          )
-        );
-        alert({
-          type: "success",
-          title: "Deleted!",
-          text: "Selected category's have been deleted.",
-        });
-      } else {
-        alert({ type: "danger", title: "Error!", text: error });
-      }
-    } catch (error) {
-      alert({ type: "danger", title: "Error!", text: error.message });
-    } finally {
-      setLoading(false);
-      setModalOpen(false);
-      setSelectedCategories([]);
-    }
-  };
 
   useLayoutEffect(() => {
     if (!auth.isSuperAdmin) navigate("/dashboard");
@@ -151,16 +85,6 @@ export default function CategoryList() {
           <div className="card-header">
             <h3 className="card-title">All Categories</h3>
             <div className="card-options">
-              {selectedCategories.length ? (
-                <button
-                  onClick={() => setModalOpen(true)}
-                  className="btn btn-danger mx-2"
-                >
-                  Delete Selected
-                </button>
-              ) : (
-                ""
-              )}
               <button
                 onClick={() => navigate("/add-category")}
                 className="btn btn-primary "
@@ -189,13 +113,6 @@ export default function CategoryList() {
           </div>
         </div>
       </div>
-
-      <ConfirmationModal
-        isOpen={modalOpen}
-        onClose={() => setModalOpen(false)}
-        onConfirm={deleteCategoryList}
-        message={`Are you sure you want to delete selected enquiry? This action cannot be undone.`}
-      />
     </div>
   );
 }
