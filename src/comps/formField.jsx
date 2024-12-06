32c332ee4454ee12dde057d60b218718bf80da7d
vishwajeetpatil5446 <vishwajeet.patil@dreamsinternational.in>
2024-12-06 11:20:22 +0530

    changes for edit

--- src/comps/formField.jsx
+++ src/comps/formField.jsx
@@ -1,37 +1,38 @@
 const FormField = ({
-    label,
-    value,
-    onChange,
-    isInvalid = false,
-    errorMessage = "",
-    type = "text",
-    placeholder = "",
-  }) => {
-    return (
-      <div className="mb-3">
-        <label className="form-label required">{label}</label>
-        {type === "textarea" ? (
-          <textarea
-            className={`form-control ${isInvalid ? "is-invalid" : ""}`}
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-            placeholder={placeholder}
-          />
-        ) : (
-          <input
-            type={type}
-            className={`form-control ${isInvalid ? "is-invalid" : ""}`}
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-            placeholder={placeholder}
-          />
-        )}
-        {isInvalid && errorMessage && (
-          <div className="invalid-feedback mt-2">{errorMessage}</div>
-        )}
-      </div>
-    );
-  };
-  
-  export default FormField;
-  
+  label,
+  value,
+  onChange,
+  isInvalid = false,
+  errorMessage = "",
+  type = "text",
+  placeholder = "",
+  isDisabled = false,
+}) => {
+  return (
+    <div className="mb-3">
+      <label className="form-label required">{label}</label>
+      {type === "textarea" ? (
+        <textarea
+          className={`form-control ${isInvalid ? "is-invalid" : ""}`}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          placeholder={placeholder}
+        />
+      ) : (
+        <input
+          type={type}
+          className={`form-control ${isInvalid ? "is-invalid" : ""}`}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          placeholder={placeholder}
+          disabled={isDisabled}
+        />
+      )}
+      {isInvalid && errorMessage && (
+        <div className="invalid-feedback mt-2">{errorMessage}</div>
+      )}
+    </div>
+  );
+};
+
+export default FormField;
--- src/pages/site/addsite.jsx
+++ src/pages/site/addsite.jsx
@@ -560,19 +560,47 @@ export default function AddSite() {
                 toggleState={siteDetails.sendCRM}
                 onToggle={(e) => handleToggle(e.target.checked, "sendCRM")}
               >
-                <FormField
-                  label="Client Id"
-                  placeholder="Client Id"
-                  value={siteDetails.sendCRMData?.clientId ?? ""}
-                  onChange={(value) =>
-                    setSiteDetails((prev) => ({
-                      ...prev,
-                      sendCRMData: { ...prev.sendCRMData, clientId: value },
-                    }))
-                  }
-                  isInvalid={!!errors.clientId}
-                  errorMessage={errors.clientId}
-                />
+                <div className="crm-clientid-container">
+                  <FormField
+                    label="Client Id"
+                    placeholder="Client Id"
+                    value={siteDetails.sendCRMData?.clientId ?? ""}
+                    onChange={(value) =>
+                      setSiteDetails((prev) => ({
+                        ...prev,
+                        sendCRMData: { ...prev.sendCRMData, clientId: value },
+                      }))
+                    }
+                    isInvalid={!!errors.clientId}
+                    errorMessage={errors.clientId}
+                  />
+                  <div className="info-icon">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24px"
+                      viewBox="0 -960 960 960"
+                      width="24px"
+                      fill="blue"
+                    >
+                      <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+                    </svg>
+                  </div>
+                  <div className="info-container">
+                    <small>Use These Url In API Condole </small>
+                    <FormField
+                      label="Homepage URL"
+                      placeholder="Homepage URL"
+                      value={import.meta.env.VITE_URL}
+                      isDisabled={true}
+                    />
+                    <FormField
+                      label="Authorized Redirect URIs"
+                      placeholder="Authorized Redirect URIs"
+                      value={import.meta.env.VITE_API_URL}
+                      isDisabled={true}
+                    />
+                  </div>
+                </div>
 
                 <FormField
                   label="Client Secret"
