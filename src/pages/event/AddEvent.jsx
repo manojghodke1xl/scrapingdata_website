d92f42bf868373370642d8db0b27b77a1760776a
NikhilDreams <nikhil.kamble@dreamsinternational.in>
2025-02-06 13:53:59 +0530

    feat: add timezone selection dropdown in AddEvent component

--- src/pages/event/AddEvent.jsx
+++ src/pages/event/AddEvent.jsx
@@ -11,6 +11,89 @@ import { showNotification } from '../../utils/showNotification';
 import { addEventApi, getEventByIdApi, updateEventApi } from '../../apis/event-apis';
 import ToggleComponent from '../../atoms/formFields/ToggleComponent';
 
+var tzInts = [
+  { name: '(GMT-12:00) International Date Line West', value: '-12' },
+  { name: '(GMT-11:00) Midway Island, Samoa', value: '-11' },
+  { name: '(GMT-10:00) Hawaii', value: '-10' },
+  { name: '(GMT-09:00) Alaska', value: '-9' },
+  { name: '(GMT-08:00) Pacific Time (US & Canada)', value: '-8' },
+  { name: '(GMT-08:00) Tijuana, Baja California', value: '-8' },
+  { name: '(GMT-07:00) Arizona', value: '-7' },
+  { name: '(GMT-07:00) Chihuahua, La Paz, Mazatlan', value: '-7' },
+  { name: '(GMT-07:00) Mountain Time (US & Canada)', value: '-7' },
+  { name: '(GMT-06:00) Central America', value: '-6' },
+  { name: '(GMT-06:00) Central Time (US & Canada)', value: '-6' },
+  { name: '(GMT-05:00) Bogota, Lima, Quito, Rio Branco', value: '-5' },
+  { name: '(GMT-05:00) Eastern Time (US & Canada)', value: '-5' },
+  { name: '(GMT-05:00) Indiana (East)', value: '-5' },
+  { name: '(GMT-04:00) Atlantic Time (Canada)', value: '-4' },
+  { name: '(GMT-04:00) Caracas, La Paz', value: '-4' },
+  { name: '(GMT-04:00) Manaus', value: '-4' },
+  { name: '(GMT-04:00) Santiago', value: '-4' },
+  { name: '(GMT-03:30) Newfoundland', value: '-3.5' },
+  { name: '(GMT-03:00) Brasilia', value: '-3' },
+  { name: '(GMT-03:00) Buenos Aires, Georgetown', value: '-3' },
+  { name: '(GMT-03:00) Greenland', value: '-3' },
+  { name: '(GMT-03:00) Montevideo', value: '-3' },
+  { name: '(GMT-02:00) Mid-Atlantic', value: '-2' },
+  { name: '(GMT-01:00) Cape Verde Is.', value: '-1' },
+  { name: '(GMT-01:00) Azores', value: '-1' },
+  { name: '(GMT+00:00) Casablanca, Monrovia, Reykjavik', value: '0' },
+  { name: '(GMT+00:00) Greenwich Mean Time : Dublin, Edinburgh, Lisbon, London', value: '0' },
+  { name: '(GMT+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna', value: '1' },
+  { name: '(GMT+01:00) Belgrade, Bratislava, Budapest, Ljubljana, Prague', value: '1' },
+  { name: '(GMT+01:00) Brussels, Copenhagen, Madrid, Paris', value: '1' },
+  { name: '(GMT+01:00) Sarajevo, Skopje, Warsaw, Zagreb', value: '1' },
+  { name: '(GMT+01:00) West Central Africa', value: '1' },
+  { name: '(GMT+02:00) Amman', value: '2' },
+  { name: '(GMT+02:00) Athens, Bucharest, Istanbul', value: '2' },
+  { name: '(GMT+02:00) Beirut', value: '2' },
+  { name: '(GMT+02:00) Cairo', value: '2' },
+  { name: '(GMT+02:00) Harare, Pretoria', value: '2' },
+  { name: '(GMT+02:00) Helsinki, Kyiv, Riga, Sofia, Tallinn, Vilnius', value: '2' },
+  { name: '(GMT+02:00) Jerusalem', value: '2' },
+  { name: '(GMT+02:00) Minsk', value: '2' },
+  { name: '(GMT+02:00) Windhoek', value: '2' },
+  { name: '(GMT+03:00) Kuwait, Riyadh, Baghdad', value: '3' },
+  { name: '(GMT+03:00) Moscow, St. Petersburg, Volgograd', value: '3' },
+  { name: '(GMT+03:00) Nairobi', value: '3' },
+  { name: '(GMT+03:00) Tbilisi', value: '3' },
+  { name: '(GMT+03:30) Tehran', value: '3.5' },
+  { name: '(GMT+04:00) Abu Dhabi, Muscat', value: '4' },
+  { name: '(GMT+04:00) Baku', value: '4' },
+  { name: '(GMT+04:00) Yerevan', value: '4' },
+  { name: '(GMT+04:30) Kabul', value: '4.5' },
+  { name: '(GMT+05:00) Yekaterinburg', value: '5' },
+  { name: '(GMT+05:00) Islamabad, Karachi, Tashkent', value: '5' },
+  { name: '(GMT+05:30) Sri Jayawardenapura', value: '5.5' },
+  { name: '(GMT+05:30) Chennai, Kolkata, Mumbai, New Delhi', value: '5.5' },
+  { name: '(GMT+05:45) Kathmandu', value: '5.75' },
+  { name: '(GMT+06:00) Almaty, Novosibirsk', value: '6' },
+  { name: '(GMT+06:00) Astana, Dhaka', value: '6' },
+  { name: '(GMT+06:30) Yangon (Rangoon)', value: '6.5' },
+  { name: '(GMT+07:00) Bangkok, Hanoi, Jakarta', value: '7' },
+  { name: '(GMT+07:00) Krasnoyarsk', value: '7' },
+  { name: '(GMT+08:00) Beijing, Chongqing, Hong Kong, Urumqi', value: '8' },
+  { name: '(GMT+08:00) Kuala Lumpur, Singapore', value: '8' },
+  { name: '(GMT+08:00) Irkutsk, Ulaan Bataar', value: '8' },
+  { name: '(GMT+08:00) Perth', value: '8' },
+  { name: '(GMT+08:00) Taipei', value: '8' },
+  { name: '(GMT+09:00) Osaka, Sapporo, Tokyo', value: '9' },
+  { name: '(GMT+09:00) Seoul', value: '9' },
+  { name: '(GMT+09:00) Yakutsk', value: '9' },
+  { name: '(GMT+09:30) Adelaide', value: '9.5' },
+  { name: '(GMT+09:30) Darwin', value: '9.5' },
+  { name: '(GMT+10:00) Brisbane', value: '10' },
+  { name: '(GMT+10:00) Canberra, Melbourne, Sydney', value: '10' },
+  { name: '(GMT+10:00) Hobart', value: '10' },
+  { name: '(GMT+10:00) Guam, Port Moresby', value: '10' },
+  { name: '(GMT+10:00) Vladivostok', value: '10' },
+  { name: '(GMT+11:00) Magadan, Solomon Is., New Caledonia', value: '11' },
+  { name: '(GMT+12:00) Auckland, Wellington', value: '12' },
+  { name: '(GMT+12:00) Fiji, Kamchatka, Marshall Is.', value: '12' },
+  { name: "(GMT+13:00) Nuku'alofa", value: '13' }
+];
+
 const AddEvent = () => {
   const navigate = useNavigate();
   const { id = '' } = useParams();
@@ -57,6 +140,7 @@ const AddEvent = () => {
     if (!eventDetails.lastBookingDate) newErrors.lastBookingDate = 'Last booking date is required';
     if (!eventDetails.venue.trim()) newErrors.venue = 'Venue is required';
     if (!eventDetails.site) newErrors.site = 'Site is required';
+    if (!eventDetails.timeZone) newErrors.timeZone = 'Timezone is required';
 
     if (eventDetails.sendAdminEmails) {
       if (!eventDetails.adminEmails.length) newErrors.adminEmails = 'At least one email is required';
@@ -201,6 +285,22 @@ const AddEvent = () => {
             <span className="block text-primary">Validity Period</span>
           </div>
           <div className="dropdown-container relative w-full mt-2">
+            <DropDown
+              mt={'mb-4'}
+              name="Time Zone"
+              dropdownList={tzInts.map((tz, index) => {
+                return { id: index, showName: tz.name, name: tz.name, value: tz.value };
+              })}
+              SummaryChild={<h5 className="p-0 m-0 text-primary">Time Zone</h5>}
+              search={true}
+              selected={eventDetails.timeZone?.label}
+              commonFunction={(e) => {
+                setEventDetails((prev) => ({ ...prev, timeZone: { label: e.name, value: e.value } }));
+                if (errors.timeZone) setErrors((prev) => ({ ...prev, timeZone: '' }));
+              }}
+              error={errors.timeZone}
+            />
+
             <DateTimePicker
               id={'date'}
               label={'Start Date'}
